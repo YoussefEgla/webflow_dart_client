92fbf22c35ae478ae60abbe8a201e06ecfb92f2a
Youssef AbouEgla <youssefegla@gmail.com>
2024-05-30 15:28:19 +0300

    [FIX]: type cast error

--- lib/src/models/page.dart
+++ lib/src/models/page.dart
@@ -62,9 +62,9 @@ class OpenGraph {
   final bool descriptionCopied;
 
   OpenGraph({
-    required this.title,
+    this.title,
     required this.titleCopied,
-    required this.description,
+    this.description,
     required this.descriptionCopied,
   });
 
